73ce87830a3cae380ee8c2a615114d012623f2ed
Thanh96 <truongthanh4196@gmail.com>
2023-02-08 11:37:23 +0700

    eslint prettier

--- tests/pages/basePage.js
+++ tests/pages/basePage.js
@@ -1,23 +1,22 @@
 import BaseAction from '../common/baseAction';
 /**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
+ * main page object containing all methods, selectors and functionality
+ * that is shared across all page objects
+ */
 export default class Page extends BaseAction {
+  /**
+   * Visit page
+   * @returns this
+   */
+  visitPage() {
+    return browser.url('/');
+  }
 
-    /**
-     * Visit page
-     * @returns this 
-     */
-    visitPage() {
-        return browser.url('/');
-    }
-
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    open(path) {
-        return browser.url(`/${path}`)
-    }
+  /**
+   * Opens a sub page of the page
+   * @param path path of the sub page (e.g. /path/to/page.html)
+   */
+  open(path) {
+    return browser.url(`/${path}`);
+  }
 }
